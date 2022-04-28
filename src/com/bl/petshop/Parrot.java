e4029287998ac1142cdff10dded06dbab30b302b
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-28 11:34:01 +0530

    Added: parrot class file added

---
+++ src/com/bl/petshop/Parrot.java
@@ -0,0 +1,12 @@
+package com.bl.petshop;
+
+public class Parrot extends Pet {
+
+	public Parrot() {
+		this.name = "Parrot";
+		this.colour = colour.GREEN;
+	}
+	public void fly() {
+		System.out.println("Pigeon can fly.");
+	}
+}
